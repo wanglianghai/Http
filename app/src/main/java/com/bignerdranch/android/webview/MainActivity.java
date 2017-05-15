1e9c305af6f814e80c0e4bcd3efc40ab5fc1161a
wanglianghai <1540446221@qq.com>
2017-05-15 18:35:56 +0800

    Adding a WebView to Your Application

--- app/src/main/java/com/bignerdranch/android/webview/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/webview/MainActivity.java
@@ -2,6 +2,8 @@ package com.bignerdranch.android.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.web_view);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://www.baidu.com");
     }
 }
